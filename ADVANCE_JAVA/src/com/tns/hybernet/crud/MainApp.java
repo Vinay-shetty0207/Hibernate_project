e72c9dfc2919eea44fd3cfaba3538975f8e28943
Vinay-shetty0207 <vinay199702@gmail.com>
2022-04-07 09:24:43 +0530

    First Commit

---
+++ ADVANCE_JAVA/src/com/tns/hybernet/crud/MainApp.java
@@ -0,0 +1,39 @@
+package com.tns.hybernet.crud;
+
+import org.hibernate.Session;
+
+public class MainApp 
+{
+	
+	public static void main(String[] args) {
+		Session objsession=Hibernate_DBConnect.getSessionFactory().openSession();
+		
+		//CRUD operation
+		//Step 1: Create
+		
+		Users user1= new Users("vinay123","apple1234","shetty","r");
+		  
+		  objsession.beginTransaction(); 
+		  objsession.save(user1);
+		  objsession.getTransaction().commit();
+		 
+			/*
+			 * //Step 2: Retrieval Users user2=new Users(); objsession.beginTransaction();
+			 * user2=objsession.get(Users.class, 1); objsession.getTransaction().commit();
+			 * System.out.println(user2);
+			 */
+			  
+			  
+				/*
+				 * // Step3: Update Users user3=new Users(); objsession.beginTransaction();
+				 * user3=objsession.get(Users.class, 3); user3.setPassword("Mac123");
+				 * objsession.getTransaction().commit(); System.out.println(user3);
+				 * 
+				 * 
+				 * //Step 4: Delete Users user4=new Users(); objsession.beginTransaction();
+				 * user4=objsession.get(Users.class,4); objsession.delete(user4);
+				 * objsession.getTransaction().commit();
+				 */
+		 
+	}
+}
